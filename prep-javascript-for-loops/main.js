31e6b5bab5aa26c337af250b4de01d2b5b2e260d
Michael Hyland <mcjhyland@icloud.com>
2021-06-16 01:47:26 -0700

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,17 @@
+// Exercise 1
+
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+// Exercise 2
+
+for (var i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+// Exercise 3
+
+for (var i = 100; i >= 1; i--) {
+  console.log('Time till explosion: ' + i);
+}
